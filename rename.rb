ba05a4589cd00c33e505e59890fca20f908dbfc9
Jeremy Weathers <jeremy@codekindly.com>
2021-05-22 16:02:53 -0500

    Rename old and new screenshot formats in Desktop

---
+++ rename.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+require 'minitest'
+
+require_relative './screenshot'
+
+class ScreenshotTest < MiniTest::Test
+  def test_find_files_none
+    result = Screenshot.find_in_dir(File.expand_path('fixtures/already-renamed', __dir__))
+
+    assert_equal [], result
+  end
+
+  def test_find_files_some
+    result = Screenshot.find_in_dir(File.expand_path('fixtures/some-renamed', __dir__))
+
+    assert_equal ['Screenshot 2020-04-24 10.42.36(2).png', 'Screen Shot 2021-05-22 at 2.16.30 PM.png'], result
+  end
+
+  def test_new_name_new_pm
+    assert_equal '2021-04-19 14.12.27.png', Screenshot.new_name('Screen Shot 2021-04-19 at 2.12.27 PM.png')
+  end
+
+  def test_new_name_new_am
+    assert_equal '2021-04-05 11.33.36.png', Screenshot.new_name('Screen Shot 2021-04-05 at 11.33.36 AM.png')
+  end
+
+  def test_new_name_new_multiple
+    assert_equal '2020-08-27 11.45.06 (2).png', Screenshot.new_name('Screen Shot 2020-08-27 at 11.45.06 AM (2).png')
+  end
+
+  def test_new_name_old
+    assert_equal '2020-07-20 15.44.48.png', Screenshot.new_name('Screenshot 2020-07-20 15.44.48.png')
+  end
+
+  def test_new_name_old_am
+    assert_equal '2020-07-31 08.46.00.png', Screenshot.new_name('Screenshot 2020-07-31 08.46.00.png')
+  end
+
+  def test_new_name_old_multiple
+    assert_equal '2020-04-24 10.42.36 (2).png', Screenshot.new_name('Screenshot 2020-04-24 10.42.36(2).png')
+  end
+end
+
+if MiniTest.run
+  Screenshot.run(File.expand_path('~/Desktop'))
+else
+  puts 'Tests failed'
+  puts 'No real files processed'
+end
---
+++ screenshot.rb
@@ -0,0 +1,43 @@
+require 'fileutils'
+require 'time'
+
+module Screenshot
+  FILES = Regexp.new(/^Screen ?[Ss]hot (\d{4})-(\d{2})-(\d{2})[ at]+(\d{1,2})\.(\d{2})\.(\d{2}) ?(AM|PM)? ?(\(\d+\))?\.png$/)
+  SETFILE = '/usr/bin/SetFile'
+  SETFILE_EXISTS = File.exist?(SETFILE)
+
+  class << self
+    def find_in_dir(dir_name)
+      files = Dir.glob('*.png', base: dir_name)
+      files.select { |file| FILES.match(file) }
+    end
+
+    def new_name(file_name)
+      return nil unless (match = FILES.match(file_name))
+
+      hour = match[4].to_i
+      hour += 12 if match[7] == 'PM'
+      hour = hour.to_s.rjust(2, '0')
+
+      "#{match[1]}-#{match[2]}-#{match[3]} #{hour}.#{match[5]}.#{match[6]}#{' ' if match[8]}#{match[8]}.png"
+    end
+
+    def run(dir_name = nil)
+      dir_name ||= File.expand_path('~/Desktop')
+      find_in_dir(dir_name).each do |old_file_name|
+        new_file_name = new_name(old_file_name)
+        rename File.expand_path(old_file_name, dir_name), File.expand_path(new_file_name, dir_name)
+      end
+      return unless SETFILE_EXISTS
+
+      system "#{SETFILE} -a E #{dir_name}/20*.png"
+    end
+
+    private
+
+    def rename(old_file, new_file)
+      puts "Renaming #{old_file} -> #{new_file}"
+      FileUtils.mv(old_file, new_file)
+    end
+  end
+end
